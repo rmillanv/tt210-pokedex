fc0a40a67734655f4ad52866c5b90547b7cc17bd
rmillanv <rmillan6@gmail.com>
2024-10-11 10:55:44 -0500

    subir busqueda pokemon

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const pokemonlist = document.getElementById("pokemon-list")
 const pokemonDetail= document.getElementById("pokemon-detail")
 const pokemonInfo = document.getElementById("pokemon-info")
 const backButton = document.getElementById("back-button")
+const searchInput = document.getElementById("search-input")
+const searchButton = document.getElementById("search-button")
+const prevButton = document.getElementById("prev-button")
+const nextButton = document.getElementById("next-button")
+
+let currentPage = 1
+const itemsPerPage = 50
+const totalPokemons = 100
 
 async function fetchPokemonData(pokemonId) {
     const Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
@@ -40,11 +48,49 @@ backButton.addEventListener("click",()=>{
     pokemonDetail.style.display = "none"
     pokemonlist.style.display = "grid"
 })
-async function loadpokedex() {
-    for(let i=1; i<=50; i++){
+async function loadpokedex(page) {
+    pokemonlist.innerHTML=""
+    const start = (page -1)*itemsPerPage +1
+    const end = page*itemsPerPage
+    for(let i=start; i<=end; i++){
         const pokemon  = await fetchPokemonData(i)
     displaypokemon(pokemon)  
     }
+    updatePaginationButtons(page)
     return 
 }
-loadpokedex()
+async function searchPokemon() {
+    console.log(searchInput)
+    const query = searchInput.value.toLowerCase().trim()
+    
+    if(query){
+        try{
+            const pokemon = await fetchPokemonData(query)
+            pokemonlist.style.display = "none"
+            showpokemonDetail(pokemon)
+        }catch (error) {
+            alert("pokemon no encontrado, intentelo de nuevo")
+        }
+    }else{
+        alert("Ingresar un nombre o un id de pokemon")
+    }
+}
+searchButton.addEventListener("click",searchPokemon)
+
+function updatePaginationButtons(page){
+    prevButton.disabled = page === 1
+    nextButton.disabled = page === Math.floor(totalPokemons/itemsPerPage)
+}
+
+
+nextButton.addEventListener("click",()=>{
+    currentPage++
+    loadpokedex(currentPage)
+})
+prevButton.addEventListener("click",()=>{
+    if(currentPage > 1){
+        currentPage--
+        loadpokedex(currentPage)
+    }
+})
+loadpokedex(currentPage)
